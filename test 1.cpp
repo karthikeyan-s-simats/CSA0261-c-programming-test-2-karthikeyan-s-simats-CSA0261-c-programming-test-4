90b3c599de323574b2dd0170ed6c0c60d216f1f1
karthikeyan-s-simats <129312612+karthikeyan-s-simats@users.noreply.github.com>
2023-03-31 14:18:50 +0530

    Add files via upload

---
+++ test 1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+{
+
+printf[enter the num ];
+scanf(%d,&num);
+}
+// TO identify the number using recursion
+printf[find the factorial and gcd];
+scanf(%d,num);
+if(i=1,i,j,j=2)
+if(i==1) (j==2)
+}
+return 0
+}
---
+++ test 2.cpp
@@ -0,0 +1,15 @@
+void num (int*a,int*a)
+int tnep=*a
+*a=*b);
+*b=temp;
+}
+intmain(){
+int main=1,num=2
+printf(intf)
+
+scanf("%dbefore the swap;n=1,num=2;
+printf(%d;n=1 n=2;
+swap(&num1,&num2);
+printf("%d after the swap;n=1,n=2);
+}
+
